99f1aeca0d7785a1c7a9b70b4d0efbf60b2ce88c
Deepak <deepak.kumar@crecientech.com>
2025-02-12 11:09:22 +0530

    updated token for dev
    updated token for dev

--- frontend/src/providers/not-authenticated.tsx
+++ frontend/src/providers/not-authenticated.tsx
@@ -14,7 +14,7 @@ export function NotAuthenticated() {
       role: "admin",
       email: "admin@resolve.com",
       token:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiI2MyIsInVzZXJfaWQiOiIxIiwicm9sZV9pZCI6IjI4OSIsInVzZXJfdHlwZV9pZCI6IjMiLCJuYmYiOjE3Mzg3MzIyNjgsImV4cCI6MTc0MDk2MDAwMCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDMifQ.msVnlMWMiM0jCdzMDPVEr5Q6QaY0uJghuWlxaspMGoU",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiI2MyIsInVzZXJfaWQiOiIxIiwicm9sZV9pZCI6IjI4OSIsInVzZXJfdHlwZV9pZCI6IjMiLCJuYmYiOjE3MzYxNTUxMTUsImV4cCI6MTc0MDk2MDAwMCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDMifQ.FO-llsxzZ8p4gxwufd9bkcn_WbHHmi2ijLJQTctyeDc",
     };
 
     // ORG - 63 | 70
